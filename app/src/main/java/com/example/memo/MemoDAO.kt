a03c907eccb81a20d2b89a46fbee520b34882125
flowershy <yxddt12@naver.com>
2022-09-29 00:42:58 +0900

    home - 2022-09-29 AM 12:39

--- app/src/main/java/com/example/memo/MemoDAO.kt
+++ app/src/main/java/com/example/memo/MemoDAO.kt
@@ -1,22 +1,22 @@
 package com.example.memo
 
-//import androidx.room.Dao
-//import androidx.room.Delete
-//import androidx.room.Insert
-//import androidx.room.OnConflictStrategy.REPLACE
-//import androidx.room.Query
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
 
 
-//@Dao
-//interface MemoDAO {
-//
-//    //@Insert(onConflict = REPLACE)   // insert 진행시 primaryKey가 중복시 1번 / 나중에 insert진행시 또 1번이 나오면 중복된다
-//    @Insert(onConflict = REPLACE)
-//    //fun insert(memo : MemoEntity)
-//
-//    @Query("SELECT * FROM memo")
-//    //fun getAll() : List<MemoEntity>
-//
-//    @Delete
-//    //fun delete(memo : MemoEntity)
-//}
+@Dao
+interface MemoDAO {
+
+    //@Insert(onConflict = REPLACE)   // insert 진행시 primaryKey가 중복시 1번 / 나중에 insert진행시 또 1번이 나오면 중복된다
+    @Insert(onConflict = REPLACE)
+    fun insert(memo : MemoEntity)
+
+    @Query("SELECT * FROM memo")
+    fun getAll() : List<MemoEntity>
+
+    @Delete
+    fun delete(memo : MemoEntity)
+}
